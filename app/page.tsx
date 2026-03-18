b8d91e7a902d2245f9aa9576feac2a61d8784835
HimashaNethmini <himashanethmini05@gmail.com>
2026-03-18 13:48:50 +0530

    removed unwanted texts

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-
-
+import Queue from "./queue/page"
 
 const page = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1 className="text-4xl font-bold text-slate-700">
-        Welcome to ClinicQ - Your Queue Management Solution!
-      </h1>
-    </div>
+    <Queue />
   )
 }
 
